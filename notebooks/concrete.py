e8df397811639a9410519dc30712c64b0f8c8589
sjladams <steven.adams.95@hotmail.com>
2024-06-11 12:37:28 +0200

    fix import

--- notebooks/concrete.py
+++ notebooks/concrete.py
@@ -1,10 +1,11 @@
 import os
-from uci_utils import runner
 
 DATASET = 'concrete'
 BASIS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 OUT_DIR = f"{BASIS_DIR}{os.sep}exp{os.sep}{DATASET}"
 print(OUT_DIR)
+
+from uci_utils import runner
 
 
 if __name__ == '__main__':
